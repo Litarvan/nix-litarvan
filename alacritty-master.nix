e517086c33ca40f49479b5a63ef72ce94c605180
Adrien Navratil <adrien1975@live.fr>
2019-11-30 16:17:04 +0100

    Updated Alacritty

--- alacritty-master.nix
+++ alacritty-master.nix
@@ -1,7 +1,41 @@
+/*{ stdenv,
+  lib,
+  fetchFromGitHub,
+  rustPlatform,
+
+  cmake,
+  gzip,
+  makeWrapper,
+  ncurses,
+  pkgconfig,
+  python3,
+
+  expat,
+  fontconfig,
+  freetype,
+  libGL,
+  libX11,
+  libXcursor,
+  libXi,
+  libXrandr,
+  libXxf86vm,
+  libxcb,
+  libxkbcommon,
+  wayland,
+  xdg_utils,
+
+  # Darwin Frameworks
+  AppKit,
+  CoreGraphics,
+  CoreServices,
+  CoreText,
+  Foundation,
+  OpenGL }:*/
+
 with import <nixpkgs> {};
 with pkgs;
-with xorg;
 with rustPlatform;
+with xorg;
 
 let
   rpathLibs = [
@@ -21,16 +55,16 @@ let
   ];
 in buildRustPackage rec {
   pname = "alacritty";
-  version = "0.3.4-d741d3";
+  version = "0.4.0";
 
   src = fetchFromGitHub {
     owner = "jwilm";
     repo = pname;
-    rev = "d741d3817debe9fdd4030bede3e4c8ca84ad078a";
-    sha256 = "0ki6agivxzj3ix3bb0n24bzy98h425553iv4f5j0gng82gjz5kdb";
+    rev = "v${version}";
+    sha256 = "0adaqdbma6gskb2g14yscrgr9gch5wf2g2clchplv72c2qr1k427";
   };
 
-  cargoSha256 = "1c4h9nq6j77zcmm2514nlgiyiryya5cdz3yjh2kq81yj8vxbcclm";
+  cargoSha256 = "1r267g8f986nxh8ms5yhp50qy1yl8gly2jr78p738qqc6frlxlhv";
 
   nativeBuildInputs = [
     cmake
@@ -45,16 +79,14 @@ in buildRustPackage rec {
     ++ lib.optionals stdenv.isDarwin [ AppKit CoreGraphics CoreServices CoreText Foundation OpenGL ];
 
   outputs = [ "out" "terminfo" ];
-  postPatch = ''
-    substituteInPlace alacritty/src/config/mouse.rs \
-      --replace xdg-open ${xdg_utils}/bin/xdg-open
-  '';
 
   postBuild = lib.optionalString stdenv.isDarwin "make app";
 
   installPhase = ''
     runHook preInstall
+
     install -D target/release/alacritty $out/bin/alacritty
+
   '' + (if stdenv.isDarwin then ''
     mkdir $out/Applications
     cp -r target/release/osx/Alacritty.app $out/Applications/Alacritty.app
@@ -63,15 +95,19 @@ in buildRustPackage rec {
     install -D extra/logo/alacritty-term.svg $out/share/icons/hicolor/scalable/apps/Alacritty.svg
     patchelf --set-rpath "${stdenv.lib.makeLibraryPath rpathLibs}" $out/bin/alacritty
   '') + ''
+
     install -D extra/completions/_alacritty -t "$out/share/zsh/site-functions/"
     install -D extra/completions/alacritty.bash -t "$out/etc/bash_completion.d/"
     install -D extra/completions/alacritty.fish -t "$out/share/fish/vendor_completions.d/"
+
     install -dm 755 "$out/share/man/man1"
     gzip -c extra/alacritty.man > "$out/share/man/man1/alacritty.1.gz"
+
     install -dm 755 "$terminfo/share/terminfo/a/"
     tic -x -o "$terminfo/share/terminfo" extra/alacritty.info
     mkdir -p $out/nix-support
     echo "$terminfo" >> $out/nix-support/propagated-user-env-packages
+
     runHook postInstall
   '';
 
@@ -81,7 +117,8 @@ in buildRustPackage rec {
     description = "GPU-accelerated terminal emulator";
     homepage = https://github.com/jwilm/alacritty;
     license = with licenses; [ asl20 ];
-    maintainers = with maintainers; [ mic92 ];
+    maintainers = with maintainers; [ filalex77 mic92 ];
     platforms = [ "x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" ];
   };
 }
+
