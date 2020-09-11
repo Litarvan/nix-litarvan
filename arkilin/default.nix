7632c613dd5becba344843875bfd1d88aeca7934
Adrien Navratil <adrien1975@live.fr>
2020-09-11 05:33:19 +0200

    Moved Spectacle and QBittorrent back to common config

--- arkilin/default.nix
+++ arkilin/default.nix
@@ -2,7 +2,6 @@
 
 {
     home.packages = with pkgs; [
-        spectacle qbitorrent
         arc-kde-theme papirus-icon-theme
     ];
 }
--- home.nix
+++ home.nix
@@ -15,7 +15,7 @@ in
     nodejs gradle adoptopenjdk-bin rustup gcc m4 gnumake binutils docker-compose
     powerline-fonts roboto source-code-pro dejavu_fonts noto-fonts-emoji jetbrains-mono
     unstable.idea.clion unstable.idea.idea-ultimate  android-studio
-    discord krita vlc obs-studio
+    discord krita spectacle vlc obs-studio qbittorrent
   ];
 
   programs = {
